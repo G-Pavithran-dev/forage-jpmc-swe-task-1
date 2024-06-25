b27de17b1854399b5a153d53ba561a2d36903f9b
G-Pavithran-dev <pavithranadithya1441@gmail.com>
2024-06-25 16:39:23 +0530

    chore: Add unit tests for getRatio function and update getDataPoint function

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -15,11 +15,19 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price']+quote['top_ask']['price'])/2))
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_priceAZero(self):
+    self.assertEqual(getRatio(0, 100), 0)
 
-
+  def test_getRatio_priceBZero(self):
+    self.assertIsNone(getRatio(100, 0), None)
+  
+  def test_getRatio_priceAEqualPriceB(self):
+    self.assertEqual(getRatio(100, 100), 1)
 
 if __name__ == '__main__':
     unittest.main()
